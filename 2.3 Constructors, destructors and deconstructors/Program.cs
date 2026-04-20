6fab00446f7075a3ae95953b0f2b071b893b1920
asebes-nix <andras.sebes@nixs.com>
2026-04-20 17:56:14 +0200

    feat: complete 2.3 Constructors, destructors and deconstructors messenger exercise

---
+++ 2.3 Constructors, destructors and deconstructors/Program.cs
@@ -0,0 +1,11 @@
+﻿using Nix.Oop.Constructors;
+
+Messenger myMessenger = new("Hey! Just testing the Cunsturctors.", 1);
+Console.WriteLine($"Status: {Messenger.State}");
+
+var(text, sequence) = myMessenger;
+
+Console.WriteLine($"Message: {text}");
+Console.WriteLine($"#{sequence} - {text}");
+
+Console.ReadKey();
---
+++ 2.3 Constructors, destructors and deconstructors/messenger.cs
@@ -0,0 +1,25 @@
+﻿namespace Nix.Oop.Constructors;
+
+public class Messenger
+{
+    public static string State;
+    public string message;
+    public int messageCounter;
+
+    static Messenger()
+    {
+        State = "Active";
+    }
+
+    public Messenger(string message, int messageCounter)
+    {
+        this.message = message;
+        this.messageCounter = messageCounter;
+    }
+
+    internal void Deconstruct(out string msg, out int counter)
+    {
+        msg = message;
+        counter = messageCounter;
+    }
+}
