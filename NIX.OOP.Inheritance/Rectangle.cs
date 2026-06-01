107bfddc82f27e0239df026357bc9008b2a9c034
asebes-nix <andras.sebes@nixs.com>
2026-06-01 17:48:08 +0200

    feat: complete 2.7 inheritance exercise

---
+++ NIX.OOP.Inheritance/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿namespace NIX.OOP.Inheritance;
+
+public class Rectangle : Shape
+{
+    public Rectangle(double width, double height) : base(width, height)
+    {
+
+    }
+
+    public new void GetArea()
+    {
+        Console.WriteLine($"Rectangle Area: {Width * Height}");
+    }
+}
---
+++ NIX.OOP.Inheritance/Shape.cs
@@ -0,0 +1,18 @@
+﻿namespace NIX.OOP.Inheritance;
+
+public class Shape
+{
+    public double Width;
+    public double Height;
+
+    public Shape(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }   
+
+    public void GetArea()
+    {
+        Console.WriteLine($"Shape Area: {Width * Height}");
+    }
+}
