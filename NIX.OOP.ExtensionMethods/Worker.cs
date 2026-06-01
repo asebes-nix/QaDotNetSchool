89f6d8d161613acb07b1a8a5bef9fffc525ad6b3
asebes-nix <andras.sebes@nixs.com>
2026-06-01 16:32:12 +0200

    feat: complete 2.6 extension method practice

---
+++ NIX.OOP.ExtensionMethods/Worker.cs
@@ -0,0 +1,18 @@
+﻿namespace NIX.OOP.ExtensionMethods;
+
+public sealed class Worker
+{
+    public int Rate;
+    public int TotalHour;
+
+    public Worker(int rate, int totalHour)
+    {
+        Rate = rate;
+        TotalHour = totalHour;
+    }
+
+    public double CalculateSallary()
+    {
+        return Rate * TotalHour * 1.5; //1.5 - coefficient
+    }
+}
