68e1b03d96bf5403c24f5a1381c9b9a292f1fdc3
asebes-nix <andras.sebes@nixs.com>
2026-06-01 14:30:11 +0200

    feat: complete 2.4 encapsulation exercise

---
+++ Nix.Oop.Encapsulation/Coffee.cs
@@ -0,0 +1,44 @@
+﻿namespace Nix.Oop.Encapsulation;
+
+public class Coffee
+{
+    private string _name;
+    private int _price;
+
+    public Coffee(string name, int price)
+    {
+        _name = name;
+        _price = price;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                _name = value;
+        }
+    }
+
+    public int Price
+    {
+        get { return _price; }
+        set
+        {
+            if (value > 0)
+                _price = value;
+        }
+    }
+
+    private void DisplayInfo()
+    {
+        Console.WriteLine($"Coffee Name: {_name}, Price: {_price}");
+    }
+
+    public void PrintInfo()
+    {
+        DisplayInfo();
+        Console.WriteLine("Coffee: because adulting is hard.");
+    }
+}
