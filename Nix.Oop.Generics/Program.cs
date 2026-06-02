edf9f21c9c2fa2d16815caa43429172541250bdc
asebes-nix <andras.sebes@nixs.com>
2026-06-02 15:06:39 +0200

    feat: complete 2.10 generics exercise

---
+++ Nix.Oop.Generics/Program.cs
@@ -0,0 +1,7 @@
+﻿using Nix.Oop.Generics;
+
+User user1 = new User(1, 100);
+Provider<User> provider = new Provider<User>();
+provider.CheckBalance(user1);
+
+Console.ReadKey();
---
+++ Nix.Oop.Generics/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Nix.Oop.Generics;
+
+public class User
+{
+    private int _userId;
+    private int _balance;
+
+    public int UserId
+    {
+        get { return _userId; }
+        set { _userId = value; }
+    }
+    public int Balance
+    {
+        get { return _balance; }
+        set { _balance = value; }
+    }
+
+    public User(int userId, int balance)
+    {
+        _userId = userId;
+        _balance = balance;
+    }
+}
