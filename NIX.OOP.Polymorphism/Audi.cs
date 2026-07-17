a2a7050b038c347278ec930631451da36c4ef2aa
asebes-nix <andras.sebes@nixs.com>
2026-07-17 11:16:42 +0200

    fix: address review feedback

--- NIX.OOP.Polymorphism/Audi.cs
+++ NIX.OOP.Polymorphism/Audi.cs
@@ -7,11 +7,6 @@ public class Audi : Wv
         Console.WriteLine($"{quantity} Audi cars ready for operation");
     }
 
-    public new void GetFullInfo()
-    {
-        Console.WriteLine($"{quantity} Audi cars ready for operation and {warranty} years warranty");
-    }
-
     public new void GetFullInfo(string model)
     {
         Console.WriteLine($"{quantity} Audi cars ready for operation, model: {model}, and {warranty} years warranty");
--- NIX.OOP.Polymorphism/Program.cs
+++ NIX.OOP.Polymorphism/Program.cs
@@ -1,24 +1,24 @@
 ﻿using Nix.Oop.Polymorphism;
 
-Wv wv = new Wv();
+Car wv = new Wv();
 wv.SetQuantity(100);
 wv.SetWarranty(3);
 wv.GetQuantity();
 wv.GetFullInfo();
-wv.GetFullInfo("silver");
+((Wv)wv).GetFullInfo("silver");
 
-Toyota toyota = new Toyota();
+Car toyota = new Toyota();
 toyota.SetQuantity(150);
 toyota.SetWarranty(5);
 toyota.GetQuantity();
 toyota.GetFullInfo();
-toyota.GetFullInfo("blue");
+((Toyota)toyota).GetFullInfo("blue");
 
-Audi audi = new Audi();
+Car audi = new Audi();
 audi.SetQuantity(88);
 audi.SetWarranty(8);
 audi.GetQuantity();
 audi.GetFullInfo();
-audi.GetFullInfo("A4");
+((Wv)audi).GetFullInfo("A4");
 
 Console.ReadKey();
