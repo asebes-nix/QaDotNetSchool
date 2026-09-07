9675eeb17c2cd711774fe5455d4b3091754e9bf7
asebes-nix <andras.sebes@nixs.com>
2026-09-07 17:50:37 +0200

    feat: add Subtraction POST test for MathApi

--- Nix.NUnit.MathApi/UnitTest1.cs
+++ Nix.NUnit.MathApi/UnitTest1.cs
@@ -44,6 +44,24 @@ namespace Nix.NUnit.MathApi
             Assert.That(parsed!.Result, Is.EqualTo("5"));
         }
 
+        [Test]
+        [Category("Subtraction")]
+        public async Task Subtract_10Minus2_Returns8()
+        {
+            var requestBody = new { expr = "10-2" };
+            string json = System.Text.Json.JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync("http://api.mathjs.org/v4/", content);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var options = new System.Text.Json.JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            MathApiResponse? parsed = System.Text.Json.JsonSerializer.Deserialize<MathApiResponse>(responseBody, options);
+
+            Assert.That(parsed, Is.Not.Null);
+            Assert.That(parsed!.Result, Is.EqualTo("8"));
+        }
+
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
